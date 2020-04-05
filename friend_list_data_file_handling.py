0eb454a029c0365127f7b6a2da5448ff42ae4b71
bishal <beesalsk@gmail.com>
2020-04-05 07:43:08 +0545

    okfile handling

--- friend_list_data_file_handling.py
+++ friend_list_data_file_handling.py
@@ -9,17 +9,43 @@ def write_data(name):
     friend.write("id_num: %s\n"% id_num)
     friend.write("phone number: %s\n" %phone_number)
     friend.close()
+#my problem is :
+#name is not passing to the function in read_data
+#and read_data function is not working
+def read_data(name):
+    #name = input("enter the name to search data:")
+    #that is taken from the main method function and is carried out in read_name(name):
 
+  with open("name.txt", 'r') as friend:
+      filecontent = friend.read()
+      print(filecontent)
+
+     #another way of reading file:
+   # friend = open("name.txt",'r')
+    #if friend.mode == 'r'
+     #   contents = friend.read()
+      #  print(contents)
 def main():
-    print("WELCOME TO MUY DIARY")
-    # run = True
-    while True:
-        print("Enter exit if your want to exit")
-        name = input("Ener your friends name:")
-        if name == "exit":
-            # run = False
-            sys.exit()
+    print("WELCOME TO MY RECORDS")
+   # while option != 3:
+    #why above while loop is not working?
+    option= int(input('enter the option:\n1)to write/add the data\n2)to search the already saved data\n3)to exit the system'))
 
+    #name = input("Ener your friends name:")
+    if option == 3:
+        sys.exit()
+    elif option == 1:
+        name = input("Ener your friends name:")
         write_data(name)
+        #write_data is working properly
+    elif option == 2:
+        name = input("Ener your friends name:")
+        #input data is not passed to the read_data(name), name? how to do that?
+        #also want to implemetnt/check is that data is already saved or not
+        read_data(name)
+        #read data function is not working
+    else:
+        print("enter the valid option from the main menu:")
+        main()
 
 main()
